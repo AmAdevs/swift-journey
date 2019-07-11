cbb75fbf16ae1b8c75e3d5942399a534c035b77c
AmAdev <aaananchai@gmail.com>
2019-07-11 13:59:39 +0700

    add StackViewDemo

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Ananchai Mankhong on 11/7/2562 BE.
+//  Copyright © 2562 Ananchai Mankhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

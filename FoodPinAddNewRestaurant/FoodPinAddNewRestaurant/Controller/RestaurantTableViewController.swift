9fd6fa5d45859846731c5146e90d0a1b796fe6d3
AmAdev <aaananchai@gmail.com>
2019-07-18 15:02:02 +0700

    unwind segue

--- FoodPinAddNewRestaurant/FoodPinAddNewRestaurant/Controller/RestaurantTableViewController.swift
+++ FoodPinAddNewRestaurant/FoodPinAddNewRestaurant/Controller/RestaurantTableViewController.swift
@@ -278,6 +278,8 @@ class RestaurantTableViewController: UITableViewController {
         }
     }
     
-    
+    @IBAction func unwindToHome(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
     
 }

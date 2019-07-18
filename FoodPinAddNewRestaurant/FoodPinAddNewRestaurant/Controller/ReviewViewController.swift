4c6699ca82f46982d35d5d0814f0100c2851bbf7
AmAdev <aaananchai@gmail.com>
2019-07-18 13:33:26 +0700

    add FoodPinAddNewRestaurant

---
+++ FoodPinAddNewRestaurant/FoodPinAddNewRestaurant/Controller/ReviewViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ReviewViewController.swift
+//  FoodPinOOPcustomNavWithMapWithAnimation
+//
+//  Created by Ananchai Mankhong on 17/7/2562 BE.
+//  Copyright © 2562 Ananchai Mankhong. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet var rateButtons: [UIButton]!
+    @IBOutlet var closeButton: UIButton!
+    
+    var restaurant = Restaurant()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        backgroundImageView.image = UIImage(named: restaurant.image)
+
+        // Applying the blur effect
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        
+        let moveRightTransform = CGAffineTransform.init(translationX: 600, y: 0) // Negative is fade animation Left to end state
+        let scaleUpTransform = CGAffineTransform.init(scaleX: 5.0, y: 5.0)
+        let moveScaleTransform = scaleUpTransform.concatenating(moveRightTransform)
+        // Make the button invisible
+        for rateButton in rateButtons {
+            rateButton.transform = moveScaleTransform
+            rateButton.alpha = 0
+        }
+        
+        let moveTopTransform = CGAffineTransform.init(translationX: 0, y: -400)
+        closeButton.transform = moveTopTransform
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 2.0) {
+            
+            for i in 0...4 {
+                UIView.animate(withDuration: 0.4, delay: (1 + 0.05 ) * Double(i) , options: [], animations: {
+                    self.rateButtons[i].alpha = 1.0
+                    self.rateButtons[i].transform = .identity
+                }, completion: nil)
+            }
+            
+            UIView.animate(withDuration: 0.4) {
+                self.closeButton.transform = .identity
+            }
+
+        }
+    }
+
+
+}

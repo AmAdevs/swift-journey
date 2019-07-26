72b96634f71d223ffc63ac19b28b51e474d780e3
AmAdev <aaananchai@gmail.com>
2019-07-26 15:23:33 +0700

    prepareNotification

--- FoodPinCloudKit/FoodPinCloudKit/Controller/RestaurantTableViewController.swift
+++ FoodPinCloudKit/FoodPinCloudKit/Controller/RestaurantTableViewController.swift
@@ -88,7 +88,8 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         }
     
         
-       
+        // Prepare Notification
+        prepareNotification()
     }
     
     
@@ -310,6 +311,34 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         } else {
             return true
         }
+    }
+    
+    // Prepare Notification
+    func prepareNotification() {
+        // Make sure the restaurant array is not empty
+        if restaurants.count <= 0 {
+            return
+        }
+        
+        
+        // Pick a restaurant randomly
+        let randomNum = Int.random(in: 0..<restaurants.count)
+        let suggestedRestaurant = restaurants[randomNum]
+        
+        
+        // Create the user notification
+        let content = UNMutableNotificationContent()
+        content.title = "Restaurant Recommentation"
+        content.subtitle = "Try new food today"
+        content.body = "I recommend you to check out \(suggestedRestaurant.name!). The restaurant is one of your favorites. It is located at \(suggestedRestaurant.location!) Would you like to give it a try? "
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: "foodpin.restaurantSuggestion", content: content, trigger: trigger)
+        
+        // Schedule the notification
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
     }
     
 
